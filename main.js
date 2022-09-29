537f43939935a1822b9513a1219952c8a20632f7
abdulvakhid <buriyevabdulvohid055@gmail.com>
2022-09-29 20:48:58 +0500

    Change

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ elForm.addEventListener("submit", function (evt) {
         let newText = document.createElement("p");
         let newNumber = document.createElement("a");
         newNumber.href = "tel:[number]";
-        
+
         let newBtn = document.createElement("button");
         newBtn.textContent = "X"
         newBtn.setAttribute("onclick", "deleteItem(this)")
@@ -47,12 +47,12 @@ elForm.addEventListener("submit", function (evt) {
         newText.textContent = arr.relation;
         newNumber.textContent = arr.number;
 
-        newList.classList.add("list-unstyled", "text-center", "d-flex","flex-wrap", "justify-content-center");
-        newItem.classList.add("mb-3", "border", "border-warning","mb-4", "me-3","p-3", "rounded", "bg-light", "shadow-lg", "position-relative", "w-25");
+        newList.classList.add("list-unstyled", "text-center", "d-flex", "flex-wrap", "justify-content-center");
+        newItem.classList.add("mb-3", "border", "border-warning", "mb-4", "me-3", "py-5", "px-3", "rounded", "bg-light", "shadow-lg", "position-relative", "w-25");
         newTitle.classList.add("fs-4", "mb-3");
         newText.classList.add("mb-2", );
         newNumber.classList.add("text-decoration-none");
-        newBtn.classList.add("position-absolute","top-0", "w-25", "end-0", "border-danger", "bg-danger", "text-white",)
+        newBtn.classList.add("position-absolute", "top-0", "w-25", "end-0", "border-danger", "bg-danger", "text-white", )
 
         newItem.appendChild(newTitle);
         newItem.appendChild(newText);
@@ -60,7 +60,5 @@ elForm.addEventListener("submit", function (evt) {
         newItem.appendChild(newBtn);
         newList.appendChild(newItem);
         elContact.appendChild(newList);
-
     }
-
 })
